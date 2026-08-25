bbfe171a2393eaa0db7612cc64d10f75e3bcf23b
sajaldi <saul.alvarado@outlook.com>
2026-08-25 17:23:46 -0600

    Fix: add CSRF_TRUSTED_ORIGINS and SECURE_PROXY_SSL_HEADER for production

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,10 @@ DEBUG = os.environ.get('DEBUG', '1') == '1'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '*').split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
